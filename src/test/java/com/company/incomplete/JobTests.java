3ce3ca8f5d04a2f4eaaeb3288b756edb5129bb76
RyanCEaton <ryan_eaton@comcast.com>
2021-07-07 13:00:33 -0600

    Tests 1-4 done, i think

--- src/test/java/com/company/incomplete/JobTests.java
+++ src/test/java/com/company/incomplete/JobTests.java
@@ -1,22 +1,45 @@
 package com.company.incomplete;
 
+import com.company.Job;
+import com.company.Employer;
+import com.company.Location;
+import com.company.PositionType;
+import com.company.CoreCompetency;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class JobTests {
+
+
+    private static final Job jobOne = new Job();
+    private static final Job jobTwo = new Job();
+    private static final Job jobThree = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    private static final Job jobFour = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
     // you can rename the tests, or delete them and rewrite them from scratch, but I went ahead and provided you the names of tests I created -- I followed the writeup pretty honestly
     @Test
     public void testSettingJobId() {
-
+        Assertions.assertEquals(1, jobOne.getId());
+        Assertions.assertEquals(2, jobTwo.getId());
+        Assertions.assertFalse(jobOne.getId() == jobTwo.getId());
+        Assertions.assertTrue((jobOne.getId() + 1) == jobTwo.getId());
     }
 
     @Test
     public void testJobConstructorSetsAllFields() {
+        Assertions.assertEquals(3, jobThree.getId());
+        Assertions.assertEquals("Product tester", jobThree.getName());
+        Assertions.assertEquals("ACME", jobThree.getEmployer().toString());
+        Assertions.assertEquals("Desert", jobThree.getLocation().toString());
+        Assertions.assertEquals("Quality control", jobThree.getPositionType().toString());
+        Assertions.assertEquals("Persistence", jobThree.getCoreCompetency().toString());
 
     }
 
     @Test
     public void testJobsForEquality() {
-
+        Assertions.assertEquals(false, jobThree == jobFour);
     }
 
     // I found the instructions around the three toString() tests a little confusing, so I created three tests following the three bullet points:
